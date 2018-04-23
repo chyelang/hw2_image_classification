4396b31408608bdd7de5a0a7cb05b7b644fccb83
charles <zouxm12@outlook.com>
2018-04-23 13:21:33 +0800

    add code to print devices

--- code_buffer.py
+++ code_buffer.py
@@ -30,4 +30,5 @@
 
 from tensorflow.python.client import device_lib
 local_device_protos = device_lib.list_local_devices()
-print(x.name for x in local_device_protos)
+for x in local_device_protos:
+	print(x.name)

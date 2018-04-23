7c8e979894533a51ed2341631b61135d762eea3a
charles <zouxm12@outlook.com>
2018-04-23 15:18:51 +0800

    modified lr rate decay

--- hw2.py
+++ hw2.py
@@ -39,8 +39,8 @@ NUM_EXAMPLES_PER_EPOCH_FOR_EVAL = hw2_input.NUM_EXAMPLES_PER_EPOCH_FOR_EVAL
 
 # Constants describing the training process.
 MOVING_AVERAGE_DECAY = 0.9999     # The decay to use for the moving average.
-NUM_EPOCHS_PER_DECAY = 350.0      # Epochs after which learning rate decays.
-LEARNING_RATE_DECAY_FACTOR = 0.1  # Learning rate decay factor.
+NUM_EPOCHS_PER_DECAY = 70      # Epochs after which learning rate decays.
+LEARNING_RATE_DECAY_FACTOR = 0.2  # Learning rate decay factor.
 INITIAL_LEARNING_RATE = 0.1       # Initial learning rate.
 
 # If a model is trained with multiple GPUs, prefix all Op names with tower_name
